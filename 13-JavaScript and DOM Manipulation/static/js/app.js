b43bda1f5582e45e403179dc3ac0f37ed13c9fd6
SWL-DEV <stephaniewlin@gmail.com>
2019-12-05 02:48:59 -0800

    multi search script updated

--- 13-JavaScript and DOM Manipulation/static/js/app.js
+++ 13-JavaScript and DOM Manipulation/static/js/app.js
@@ -15,47 +15,72 @@ var tableData = data;
 
 var button = d3.select('#filter-btn');
 
-button.on("click", function() {
+button.on("click", function () {
 
   // Clear out existing table content if a previous query was run 
   d3.select("tbody").selectAll("*").remove()
 
   // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
+  var inputElement = d3.select(".form-control");
 
   // Get the value property of the input element
   var inputValue = inputElement.property("value");
 
+  // Attempting to search with multiple search boxes:
+  // // Select the input element and get the raw HTML node
+  // var inputElement = {
+  // date: d3.select("#datetime"), 
+  // city: d3.select("#city"),
+  // state: d3.select("#state"),
+  // country: d3.select("#country"),
+  // shape: d3.select("#shape"),
+  // };
+
+  // // Get the value property of the input element
+  // var inputValue = inputElement.value(); (<-- What to do here to have it equal to one of the values??)
+
   console.log(inputValue);
 
-  // Create a filter for tableData to find datetime which matches the inputValue
-  function dateFilter(input) {
-    return input.datetime === inputValue;
-  }
-  
-  var filteredData = tableData.filter(dateFilter);
-  
-  console.log(filteredData);
 
-  // Get a reference to the table body
-  var tbody = d3.select("tbody");
+  // Create a filter for tableData to find various available input which matches the inputValue:
+  function inputFilter(input) {
+    return input.datetime === inputValue ||
+      input.city === inputValue ||
+      input.state === inputValue ||
+      input.country === inputValue ||
+      input.shape === inputValue;
 
-  // Loop through the filteredData
-  filteredData.forEach((data) => {
+  };
 
-    // For each sighting, add a new row
-    var row = tbody.append("tr");
+  var filteredData = tableData.filter(inputFilter);
 
-    // For each sighting's date (key) and relevant values
-    Object.entries(data).forEach(([key, value]) => {
+  // If there is a match of keyword entered, then print data to table:
+  if (filteredData != "") {
+    console.log(filteredData);
 
-      // Add a new cell for each value
-      var cell = row.append("td");
+    // Get a reference to the table body
+    var tbody = d3.select("tbody");
+
+    // Loop through the filteredData
+    filteredData.forEach((data) => {
+
+      // For each sighting, add a new row
+      var row = tbody.append("tr");
+
+      // For each sighting's date (key) and relevant values
+      Object.entries(data).forEach(([key, value]) => {
+
+        // Add a new cell for each value
+        var cell = row.append("td");
         cell.text(value);
-
-    
+      });
     });
+  }
 
-  });
+  // If no match is found for keyword:
+  else {
+    console.log("No Matches Found");
 
+    d3.select("tbody").append("tr").append("td").text("No Matches Found");
+  };
 });
